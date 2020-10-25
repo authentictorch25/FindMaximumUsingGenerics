589936656b846e409a598f9baf23445795727591
Akash Pratap Singh <akashpratap2507@gmail.com>
2020-10-25 22:54:19 +0530

    [Akash] Added the program to find max among strings

--- Program.cs
+++ Program.cs
@@ -67,6 +67,31 @@ namespace MaxProblem
 
             return 0;
         }
+        public static string MaxAmongStrings(string firstString, string secondString, string thirdString)
+        {
+            if ((firstString.CompareTo(secondString) > 0 && firstString.CompareTo(thirdString) > 0) ||
+                (firstString.CompareTo(secondString) >= 0 && firstString.CompareTo(thirdString) > 0) ||
+                (firstString.CompareTo(secondString) > 0 && firstString.CompareTo(thirdString) >= 0))
+            {
+                return firstString;
+            }
+
+            if ((secondString.CompareTo(firstString) > 0 && secondString.CompareTo(thirdString) > 0) ||
+                (secondString.CompareTo(firstString) >= 0 && secondString.CompareTo(thirdString) > 0) ||
+                (secondString.CompareTo(firstString) > 0 && secondString.CompareTo(thirdString) >= 0))
+            {
+                return secondString;
+            }
+
+            if ((thirdString.CompareTo(secondString) > 0 && thirdString.CompareTo(firstString) > 0) ||
+                (thirdString.CompareTo(secondString) >= 0 && thirdString.CompareTo(firstString) > 0) ||
+                (thirdString.CompareTo(secondString) > 0 && thirdString.CompareTo(firstString) >= 0))
+            {
+                return thirdString;
+            }
+
+            return "0";
+        }
     }
 }
 
