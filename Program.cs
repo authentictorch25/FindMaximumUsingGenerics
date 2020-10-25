99278b2c0ea431e61c11705c1f3a0d204898cde4
Akash Pratap Singh <akashpratap2507@gmail.com>
2020-10-25 22:45:31 +0530

    [Akash] Added program to fing the max among float

--- Program.cs
+++ Program.cs
@@ -42,6 +42,31 @@ namespace MaxProblem
 
             return 0;
         }
+        public static float MaxAmongFloat(float firstValue, float secondValue, float thirdValue)
+        {
+            if ((firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0) ||
+                (firstValue.CompareTo(secondValue) >= 0 && firstValue.CompareTo(thirdValue) > 0) ||
+                (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) >= 0))
+            {
+                return firstValue;
+            }
+
+            if ((secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0) ||
+                (secondValue.CompareTo(firstValue) >= 0 && secondValue.CompareTo(thirdValue) > 0) ||
+                (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) >= 0))
+            {
+                return secondValue;
+            }
+
+            if ((thirdValue.CompareTo(secondValue) > 0 && thirdValue.CompareTo(firstValue) > 0) ||
+                (thirdValue.CompareTo(secondValue) >= 0 && thirdValue.CompareTo(firstValue) > 0) ||
+                (thirdValue.CompareTo(secondValue) > 0 && thirdValue.CompareTo(firstValue) >= 0))
+            {
+                return thirdValue;
+            }
+
+            return 0;
+        }
     }
 }
 
