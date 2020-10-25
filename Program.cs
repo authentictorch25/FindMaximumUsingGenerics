188e440cfbfb8df72fb751193e7e5cf670d02f97
Akash Pratap Singh <akashpratap2507@gmail.com>
2020-10-25 22:41:07 +0530

    [Akash] Added the program to find the largest integer

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace MaxProblem
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Max among 5, 10 and 15 is: " + MaxAmongIntegers(5, 10, 15));
+        }
+        /// <summary>
+        /// Maximums the among integers.
+        /// </summary>
+        /// <param name="firstValue">The first value.</param>
+        /// <param name="secondValue">The second value.</param>
+        /// <param name="thirdValue">The third value.</param>
+        /// <returns>
+        /// max integer
+        /// </returns>
+        public static int MaxAmongIntegers(int firstValue, int secondValue, int thirdValue)
+        {
+            if ((firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0) ||
+                (firstValue.CompareTo(secondValue) >= 0 && firstValue.CompareTo(thirdValue) > 0) ||
+                (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) >= 0))
+            {
+                return firstValue;
+            }
+
+            if ((secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0) ||
+                (secondValue.CompareTo(firstValue) >= 0 && secondValue.CompareTo(thirdValue) > 0) ||
+                (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) >= 0))
+            {
+                return secondValue;
+            }
+
+            if ((thirdValue.CompareTo(secondValue) > 0 && thirdValue.CompareTo(firstValue) > 0) ||
+                (thirdValue.CompareTo(secondValue) >= 0 && thirdValue.CompareTo(firstValue) > 0) ||
+                (thirdValue.CompareTo(secondValue) > 0 && thirdValue.CompareTo(firstValue) >= 0))
+            {
+                return thirdValue;
+            }
+
+            return 0;
+        }
+    }
+}
+
